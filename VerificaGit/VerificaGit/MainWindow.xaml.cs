64d3a45340958d553706bd4c165b168d5b40363b
Simone Ceccarelli <Simonececcarelli001@gmail.com>
2024-10-21 09:18:47 +0200

    Interfaccia v1.0

--- VerificaGit/VerificaGit/MainWindow.xaml.cs
+++ VerificaGit/VerificaGit/MainWindow.xaml.cs
@@ -19,6 +19,37 @@ namespace VerificaGit
         public MainWindow()
         {
             InitializeComponent();
+            Biblioteca biblioteca = new Biblioteca("malatesta", "Cesena", "10:00", "18:00");
+        }
+
+
+        private void CreaLibro_Click(object sender, RoutedEventArgs e)
+        {
+            
+            Libro libro = new Libro(Titolo.Text, Autore.Text, Pubblicazione.Text, Editore.Text, Pagine.Text);
+            AddLibro(libro);
+        }
+
+        private void VerificaTitolo_Click(object sender, RoutedEventArgs e)
+        {
+            if (FindByTitle(Titolo.Text))
+            {
+                Result.Text = "Presente";
+            }
+            else
+            {
+                Result.Text = "Non Presente";
+            }
+        }
+
+        private void RicercaAutore_Click(object sender, RoutedEventArgs e)
+        {
+            Result_Copia.Text = FindByAuthor(Autore.Text);
+        }
+
+        private void VerificaLibri_Click(object sender, RoutedEventArgs e)
+        {
+            Reult_Copia1.Text = Count().ToString();
         }
     }
 }
